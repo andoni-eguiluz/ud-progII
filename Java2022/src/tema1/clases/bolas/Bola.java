63803e324c8921f7e50bf2d45329df9e7de042d6
Andoni Eguíluz Morán <andoni.eguiluz@deusto.es>
2022-02-28 10:07:30 +0100

    Clase 28 feb

--- Java2022/src/tema1/clases/bolas/Bola.java
+++ Java2022/src/tema1/clases/bolas/Bola.java
@@ -5,11 +5,13 @@ import java.awt.Color;
 import utils.ventanas.ventanaBitmap.VentanaGrafica;
 
 public class Bola {
-	private int xCentro;
-	private int yCentro;
+	private double xCentro;
+	private double yCentro;
 	private int radio;
 	private Color colorBorde;
 	private Color colorFondo;
+	private double velX;
+	private double velY;
 	
 	/**
 	 * @param xCentro
@@ -40,19 +42,19 @@ public class Bola {
 		this( 0, 0, 10, Color.BLUE, new Color(255,255,255) );
 	}
 
-	public int getxCentro() {
+	public double getxCentro() {
 		return xCentro;
 	}
 
-	public void setxCentro(int xCentro) {
+	public void setxCentro(double xCentro) {
 		this.xCentro = xCentro;
 	}
 
-	public int getyCentro() {
+	public double getyCentro() {
 		return yCentro;
 	}
 
-	public void setyCentro(int yCentro) {
+	public void setyCentro(double yCentro) {
 		this.yCentro = yCentro;
 	}
 
@@ -79,8 +81,29 @@ public class Bola {
 	public void setColorFondo(Color colorFondo) {
 		this.colorFondo = colorFondo;
 	}
-
 	
+	/** Devuelve la velocidad horizontal de la bola
+	 * @return	Velocidad en pixels / segundo
+	 */
+	public double getVelX() {
+		return velX;
+	}
+
+	/** Cambia la velocidad en x de la bola
+	 * @param velX	Nueva velocidad en píxels / segundo
+	 */
+	public void setVelX(double velX) {
+		this.velX = velX;
+	}
+
+	public double getVelY() {
+		return velY;
+	}
+
+	public void setVelY(double velY) {
+		this.velY = velY;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		Bola bola2 = (Bola) obj;
@@ -102,6 +125,63 @@ public class Bola {
 		v.dibujaCirculo(this.getxCentro(), this.getyCentro(), this.getRadio(), 2, 
 				this.getColorBorde(), this.getColorFondo() );
 	}
+
+	/** Mueve la bola un tiempo indicado
+	 * @param v	Ventana en la que se dibuja
+	 * @param tiempo	Tiempo de movimiento
+	 */
+	public void mover( VentanaGrafica v, double tiempo ) {
+		borrar( v );
+		setxCentro( xCentro + velX * tiempo );
+		setyCentro( yCentro + velY * tiempo );
+		if (chocaEnVertical( v )) { // bola choca con el borde superior o inferior) {
+			velY = -velY;
+		}
+		if (chocaEnHorizontal( v )) {
+			velX = -velX;
+		}
+		dibujar( v );
+	}
 	
+	// Mover bloqueante
+	/** Mueve la bola y la rebota hasta que se cierre la ventana
+	 * @param v
+	 */
+	public void mover( VentanaGrafica v ) {
+		double tiempo = 0.01;  // segundos
+		long espera = (long) (tiempo * 1000); // milisegundos
+		double fps = 1 / tiempo;
+		System.out.println( "Tiempo sgs = " + tiempo + " / fps = " + fps );
+		while (!v.estaCerrada()) {
+			borrar( v );
+			// Muevo
+			setxCentro( xCentro + velX * tiempo );  // xCentro = xCentro + velX;   // xCentro += velX;
+			setyCentro( yCentro + velY * tiempo );
+			// Compruebo posibles choques
+			if (chocaEnVertical( v )) { // bola choca con el borde superior o inferior) {
+				velY = -velY;
+			}
+			if (chocaEnHorizontal( v )) {
+				velX = -velX;
+			}
+			dibujar( v );
+			v.espera( espera );
+		}
+	}
+	
+	public boolean chocaEnVertical( VentanaGrafica v ) {
+		if (yCentro < 0 + radio) {
+			return true;
+		}
+		if (yCentro > v.getAltura() - radio) {
+			return true;
+		}
+		return false;
+		// return (yCentro < 0) || (yCentro > v.getAltura());
+	}
+	
+	public boolean chocaEnHorizontal( VentanaGrafica v ) {
+		return (xCentro < radio) || (xCentro > v.getAnchura() - radio);
+	}
 	
 }
--- Java2022/src/tema1/clases/bolas/MainBolas.java
+++ Java2022/src/tema1/clases/bolas/MainBolas.java
@@ -5,10 +5,34 @@ import java.awt.Color;
 import utils.ventanas.ventanaBitmap.VentanaGrafica;
 
 public class MainBolas {
+	private static VentanaGrafica v;
+	private static Bola bola1;
+	private static Bola bola2;
+	
 	public static void main(String[] args) {
 		// pruebas();
-		pruebaMovimiento();
+		// pruebaMovimiento();
+		reboteBolas();
 	}
+	
+	private static void reboteBolas() {
+		v = new VentanaGrafica( 1000, 600, "Rebote de bolas" );
+		v.getJFrame().setLocation( 2000, 0 );
+		bola1 = new Bola( 500, 300, 20, Color.BLUE, Color.YELLOW );
+		bola1.dibujar( v );
+		bola1.setVelX( 500 );
+		bola1.setVelY( 200 );
+		
+		bola2 = new Bola( 200, 200, 25, Color.PINK, Color.CYAN );
+		bola2.dibujar( v );
+		bola2.setVelX( 100 );
+		bola2.setVelY( 100 );
+		
+		bola1.mover( v );  // NO DEVUELVE EL CONTROL  (BLOQUEANTE)
+		bola2.mover( v );
+		
+	}
+	
 	private static void pruebaMovimiento() {
 		VentanaGrafica v = new VentanaGrafica( 1000, 600, "Bolitas" );
 		v.getJFrame().setLocation(2000, 0);
